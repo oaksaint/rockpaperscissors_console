2d9265009bdc9797e4de7cd345032011014fd0e8
Ariel Santos <105682233+oaksaint@users.noreply.github.com>
2022-12-12 09:47:35 -0300

    Add player prompt with conversion to lower case

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 //compare player variable to computer player variable
 //print out winner
 
-
-
-
-
 // get an input from the player and assign the player input to a variable
-const getPlayerChoice = function() {
-    const playerSelection = prompt("Rock, Paper or Scissors? Which one do you choose?");
-    if (playerSelection === null || playerSelection === "") {
-        alert("You have to choose one!");
-        getPlayerChoice();
-    }
-    }
+const getPlayerChoice = function () {
+  let playerPrompt = prompt("Rock, Paper or Scissors?").toLowerCase();
+  console.log(playerPrompt);
+  return playerPrompt;
+};
 
 //randomly generate an input from 3 possible choices for the computer player
-const getComputerChoice = function() {
-    let computerSelection = Math.floor(Math.random() * 3) + 1;
-    if (computerSelection === 1) {
-        computerSelection = "rock";
-    } else if (computerSelection === 2) {
-        computerSelection = "paper";
-    } else if (computerSelection === 3) {
-        computerSelection = "scissors";
-    }
-    console.log(`Computer plays ${computerSelection}`);
-    return computerSelection;
-}
+const getComputerChoice = function () {
+  let computerSelection = Math.floor(Math.random() * 3) + 1;
+  if (computerSelection === 1) {
+    computerSelection = "rock";
+  } else if (computerSelection === 2) {
+    computerSelection = "paper";
+  } else if (computerSelection === 3) {
+    computerSelection = "scissors";
+  }
+  console.log(`Computer plays ${computerSelection}`);
+  return computerSelection;
+};
 
 getPlayerChoice();
+getComputerChoice();
