e933643228f89b0346fca2d66bb3401e1063cb3a
Ariel Santos <105682233+oaksaint@users.noreply.github.com>
2022-12-12 17:17:55 -0300

    Submit final version of Rock Paper Scissors CLI
    Added a scoring system, the final result and implemented a for loop to
    play all 5 rounds.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// declare score variables initial values
+let playerScore = 0;
+let computerScore = 0;
+
 // get a case insensitive input from the player, convert the input to lowercase and assign the player input to a variable
 const getPlayerChoice = function () {
   let playerSelection = prompt("Rock, Paper or Scissors?");
@@ -12,7 +16,6 @@ const getPlayerChoice = function () {
     alert("You have to choose one!");
     getPlayerChoice();
   } else {
-    console.log(`You play ${playerSelection}`);
     window.playerSelection = playerSelection;
   }
 };
@@ -27,34 +30,89 @@ const getComputerChoice = function () {
   } else if (computerSelection === 3) {
     computerSelection = "scissors";
   }
-  console.log(`Computer plays ${computerSelection}`);
   window.computerSelection = computerSelection;
 };
 
-//compare player variable to computer player variable and log the winner
-const playRound = function () {
+//compare player variable to computer player variable, print and log the winner
+const playRound = function (playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "rock") {
     console.log("It's a draw!");
+    let result = "draw";
+    window.result = result;
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log("You lose!");
+    console.log("You lose! Paper beats Rock!");
+    let result = "loss";
+    window.result = result;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("You win!");
+    let result = "win";
+    window.result = result;
+    console.log("You win! Rock beats Scissors!");
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("You win!");
+    let result = "win";
+    window.result = result;
+    console.log("You win! Paper beats Rock!");
   } else if (playerSelection == "paper" && computerSelection == "paper") {
+    let result = "draw";
+    window.result = result;
     console.log("It's a draw!");
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log("You lose!");
+    let result = "loss";
+    window.result = result;
+    console.log("You lose! Scissors beats Paper!");
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    console.log("You lose!");
+    let result = "loss";
+    window.result = result;
+    console.log("You lose! Rock beats Scissors!");
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("You win!");
+    let result = "win";
+    window.result = result;
+    console.log("You win! Scissors beats Paper!");
   } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+    let result = "draw";
+    window.result = result;
     console.log("It's a draw!");
   }
 };
 
-// functions running in order
-getPlayerChoice();
-getComputerChoice();
-playRound();
+// adds up the scores based on who won the round, or logs no scores if it was a draw
+const whoWon = function () {
+  if (result === "win") {
+    ++playerScore;
+    console.log(`Your score: ${playerScore} | Computer score ${computerScore}`);
+  } else if (result === "loss") {
+    ++computerScore;
+    console.log(
+      `Your score: ${playerScore} | Computer score: ${computerScore}`
+    );
+  } else {
+    console.log(`Your score: ${playerScore} | Computer score ${computerScore}`);
+  }
+};
+
+// prints out who was the winner based on who had more points in the end
+const finalScore = function () {
+  if (playerScore > computerScore) {
+    console.log(
+      `You have won the game! Final score ${playerScore}x${computerScore}.`
+    );
+  } else if (computerScore > playerScore) {
+    console.log(
+      `You have lost the game! Final score ${computerScore}x${playerScore}.`
+    );
+  } else {
+    console.log(
+      `The game ended in a draw! Final score ${computerScore}x${playerScore}.`
+    );
+  }
+};
+
+for (let i = 0; i < 5; i++) {
+  getPlayerChoice();
+  getComputerChoice();
+  console.log(
+    `You play ${playerSelection} and the computer plays ${computerSelection}`
+  );
+  playRound(playerSelection, computerSelection);
+  whoWon();
+}
+finalScore();
